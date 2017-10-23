c741279f87dc29a2a8f04bdaf10ac469d57ec7f2
Jim Derry <balthisar@gmail.com>
2017-10-22 22:27:43 -0400

    Added tests; some issues with exported symbols.

--- SwLibTidyTests/SwLibTidyTests.swift
+++ SwLibTidyTests/SwLibTidyTests.swift
@@ -16,64 +16,128 @@
 
 import XCTest
 @testable import SwLibTidy
+//import CLibTidy
 
 class SwiftTests: XCTestCase {
     
     private var tdoc: TidyDoc?
-    private let delimiter = "=-=-=-=-=-=-=-=-=-=-"
 
     override func setUp() {
         super.setUp()
-        tdoc = tidyCreate()
-        print(delimiter)
+        tdoc = SwLibTidy.tidyCreate()!
     }
     
     
     override func tearDown() {
-        print(delimiter)
-        if let tdoc = tdoc {
-            tidyRelease(tdoc)
-        }
-
+        SwLibTidy.tidyRelease( tdoc! )
         super.tearDown()
     }
 
-    /**
-     Ensure that we can set and get Tidy's app data properly. Proper storage
-     is required so that we can properly execute Tidy's callbacks, if used.
-     - tidySetAppData()
-     - tidyGetAppData()
-     */
-    func testSetAndGetAppData() {
 
-        guard let tdoc = tdoc else { return }
+    /* Many of our tests require Tidy to Tidy a file first. */
+    func tidy( doc: TidyDoc, file: String, config: String? = nil) -> Swift.Bool {
 
-        tidySetAppData(tdoc, self)
-        let gotObject = tidyGetAppData(tdoc)
+        let testBundle = Bundle(for: type(of: self))
+
+        if let config = config {
+            if let file = testBundle.path(forResource: config, ofType: "conf") {
+                let _ = SwLibTidy.tidyLoadConfig( doc, file )
+            }
+        }
+
+        if let file = testBundle.path(forResource: file, ofType: "html") {
+           let _ = SwLibTidy.tidyParseFile( doc, file )
+        } else {
+            return false
+        }
+
+        return true
+    }
+
+
+    /*************************************************************************
+      Although the setUp() and tearDown() do this for every test, let's test
+      it for the sake of coverage.
+     *************************************************************************/
+    func test_tidyCreate() {
+
+        if let localDoc = SwLibTidy.tidyCreate() {
+            SwLibTidy.tidyRelease( localDoc )
+        } else {
+            XCTAssert ( false, "Could not create a Tidy document." )
+        }
+    }
+
+
+    /*************************************************************************
+      Ensure that we can set and get Tidy's app data properly. Proper storage
+      is required so that we can properly execute Tidy's callbacks, if used.
+      - tidySetAppData()
+      - tidyGetAppData()
+     *************************************************************************/
+    func test_tidySetAppData_tidyGetAppData() {
+
+        SwLibTidy.tidySetAppData( tdoc!, self )
+        let gotObject = SwLibTidy.tidyGetAppData( tdoc! )
     
-        XCTAssert(gotObject === self, "The object stored is not that same as the object retrieved.")
+        XCTAssert( gotObject === self, "The object stored is not that same as the object retrieved." )
     }
     
     
-    /**
-     Test basic function:
-     - tidyReleaseDate()
-     */
-    func test_tidyReleaseDate() {
-        let str = tidyReleaseDate()
-        print(str)
-        XCTAssert(str.hasPrefix("2017."), "The release date does not begin with 2017.")
+    /*************************************************************************
+      Test basic function:
+      - tidyReleaseDate()
+      - tidyLibraryVersion()
+      - tidyPlatform()
+     *************************************************************************/
+    func test_tidyReleaseInformation() {
+
+        XCTAssert( SwLibTidy.tidyReleaseDate().hasPrefix("2017."), "The release date does not begin with 2017." )
+
+        XCTAssert( SwLibTidy.tidyLibraryVersion().hasPrefix("5.5"), "The library version does not begin with 5.5." )
+
+        XCTAssert( (SwLibTidy.tidyPlatform()?.hasPrefix("Apple"))!, "The platform does not begin with \"Apple\"" )
     }
     
     
-    /**
-     Test basic function:
-     - tidyLibraryVersion()
-     */
-    func test_tidyLibraryVersion() {
-        let str = tidyLibraryVersion()
-        print(str)
-        XCTAssert(str.hasPrefix("5.5"), "The library version does not begin with 5.5.")
+    /*************************************************************************
+      Test basic function:
+      - tidyStatus()
+      - tidyDetectedXhtml()
+      - tidyDetectedGenericXml()
+      - tidyErrorCount()
+      - tidyWarningCount()
+      - tidyAccessWarningCount()
+      - tidyConfigErrorCount()
+      - tidyLoadConfig()
+     *************************************************************************/
+    func test_tidyStatusInformation() {
+
+        XCTAssert( tidy( doc: tdoc!, file: "case-001", config: "case-001" ), "Could not locate the file for testing." )
+
+        XCTAssert( SwLibTidy.tidyStatus( tdoc! ) == 1, "Expected tidyStatus() == 1" )
+
+        XCTAssert( SwLibTidy.tidyDetectedXhtml( tdoc! ) == false, "Expected tidyDetectedXhtml() == false" )
+
+        XCTAssert( SwLibTidy.tidyDetectedGenericXml( tdoc! ) == false, "Expected tidyDetectedGenericXml() == false" )
+
+        XCTAssert( SwLibTidy.tidyErrorCount( tdoc! ) == 0, "Expected tidyErrorCount() == 0" )
+
+        XCTAssert( SwLibTidy.tidyWarningCount( tdoc! ) == 3, "Expected tidyWarningCount() == 3" )
+
+        XCTAssert( SwLibTidy.tidyAccessWarningCount( tdoc! ) == 4, "Expected tidyAccessWarningCount() == 4" )
+
+        XCTAssert( SwLibTidy.tidyConfigErrorCount( tdoc! ) == 1, "Expected tidyConfigErrorCount() == 1" )
+
+        /*
+         Prove that we loaded a config file by checking the value of
+         TidyAccessibilityCheckLevel, thus, tidyLoadConfig() worked.
+         */
+
+        let x: TidyOptionId = TidyVertSpace // how to re-export this symbol without needing the CLibTidy?
+        XCTAssert( SwLibTidy.tidyOptGetInt( tdoc!, x ) == 3, "Expected " )
+
+
     }
 
 }
