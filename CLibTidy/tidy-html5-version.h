24e283c9099068c6eab2cc192137c67d8d960976
Jim Derry <balthisar@gmail.com>
2017-10-26 15:23:32 -0400

    Updated HTML Tidy version.

--- CLibTidy/tidy-html5-version.h
+++ CLibTidy/tidy-html5-version.h
@@ -7,5 +7,5 @@
 #ifdef RELEASE_DATE
 #undef RELEASE_DATE
 #endif
-#define LIBTIDY_VERSION "5.5.71"
-#define RELEASE_DATE "2017.10.25"
+#define LIBTIDY_VERSION "5.5.72"
+#define RELEASE_DATE "2017.10.26"
